2a66718ee18c882b53ce9cf9ea4a83e43927bfe9
calvine <calvin.echols@gmail.com>
2021-10-29 00:05:18 -0400

    toying around

--- main.go
+++ main.go
@@ -5,17 +5,41 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"unicode/utf8"
 )
 
 type inputArgs struct {
 	inputEmailFile string
 }
 
+type ParserState uint8
+
+const (
+	Unset ParserState = iota
+	AlphaNumeric
+	WhiteSpace
+	CarrageReturn
+	LineFeed
+)
+
+const (
+	cr  = '\r'
+	lf  = '\n'
+	sp  = ' '
+	tab = '\t'
+
+	colon = ':'
+)
+
+var (
+	parserState = Unset
+)
+
 func main() {
 	var inputArgs inputArgs
 	flag.StringVar(&inputArgs.inputEmailFile, "inputFile", "", "The input email file for DKIM signature verification")
 	flag.Parse()
-	fmt.Printf("test call %v\n\n", inputArgs)
+	fmt.Printf("test call %v\n", inputArgs)
 	fileData, err := ioutil.ReadFile(inputArgs.inputEmailFile)
 	if err != nil {
 		fmt.Printf("failed to open file %s: %s", inputArgs.inputEmailFile, err.Error())
@@ -24,9 +48,22 @@ func main() {
 	}
 	fileLength := len(fileData)
 	if fileLength <= 0 {
-		fmt.Printf("file contains no data!")
+		fmt.Print("file contains no data!\n")
 		os.Exit(2)
 		return
 	}
 	fmt.Printf("file length: %d\n", fileLength)
+	for i := 0; i < 150; {
+		// decode text as UTF-8 to handle a wide variety of characters in text?
+		r, width := utf8.DecodeRune(fileData[i : i+1])
+		fmt.Println(i, "\t", r, "\t", string(r))
+		i += width
+	}
 }
+
+// func ByteIsNonCRLFWhitespace(b byte) bool {
+// 	switch b {
+// 	case '\t':
+
+// 	}
+// }
