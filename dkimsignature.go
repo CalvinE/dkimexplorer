cc22573319cbd166ad7050d36ec0930980dcecc9
calvine <calvin.echols@gmail.com>
2021-11-07 21:06:29 -0500

    moved some code

--- dkimsignature.go
+++ dkimsignature.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"crypto"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"errors"
 	"fmt"
+	"log"
+	"net"
 	"strconv"
 	"strings"
 	"unicode/utf8"
@@ -284,4 +293,148 @@ func GetDKIMSignatureForVerificationOrSigningBytes(sigBytes []byte) []byte {
 	}
 	// TODO: whi is the CRLF gone?
 	return prepedDKIMSIgnature
+}
+
+func validateDKIMSignature(message *message, dkimSignatureToValidate DKIMSignature, otherDKIMSignatures []header) (DKIMValidationState, error) {
+	// calculate canonicalized body
+	canonicalizeBodyBytes, err := canonicalizeBody(message, dkimSignatureToValidate.CanonicalizationAlgorithm, dkimSignatureToValidate.BodyLengthLimit)
+	if err != nil {
+		fmt.Printf("Failed to canonicalize body of message: %s%s%s", err.Error(), CRLF, CRLF)
+		return PERMFAIL, err
+	}
+	// calculate hash on canonicalized body hash
+	var canonicalizedBodyHash []byte
+	var canonicalizedBodyHashBase64 string
+
+	switch dkimSignatureToValidate.Algorithm {
+	case DKIM_ALGORITHM_RSA_SHA_1:
+		hash := sha1.Sum(canonicalizeBodyBytes)
+		canonicalizedBodyHash = hash[:]
+		canonicalizedBodyHashBase64 = base64.StdEncoding.EncodeToString(canonicalizedBodyHash)
+	case DKIM_ALGORITHM_RSA_SHA_256:
+		hash := sha256.Sum256(canonicalizeBodyBytes)
+		canonicalizedBodyHash = hash[:]
+		canonicalizedBodyHashBase64 = base64.StdEncoding.EncodeToString(canonicalizedBodyHash)
+	}
+	// confirm canonicalized body hash to dkim signature bh value
+	if canonicalizedBodyHashBase64 != dkimSignatureToValidate.BodySignature {
+		fmt.Printf("body hash does not match: got: %s - expected: %s%s%s", canonicalizedBodyHashBase64, dkimSignatureToValidate.BodySignature, CRLF, CRLF)
+		return PERMFAIL, errors.New("body hash does not validate")
+	}
+	// caluclate canonicalize headers
+	canonicalizedHeaderData, err := canonicalizeHeaders(message, dkimSignatureToValidate.CanonicalizationAlgorithm, otherDKIMSignatures, dkimSignatureToValidate.SignedHeaders)
+	if err != nil {
+		fmt.Printf("failed to canonicalize headers: %s%s%s", err.Error(), CRLF, CRLF)
+		return TEMPFAIL, err
+	}
+	// get "domain key" from set query method
+	// for now I am going to assume the value is the defaul which is "dns/txt" I dont know if ill implment others...
+	if dkimSignatureToValidate.QueryMethod != DEFAULT_QUERY_METHOD {
+		fmt.Printf("currently only one query method is supported.%s", CRLF)
+		fmt.Printf("unsupported query method encountered: got: %s - expected: %s%s%s", dkimSignatureToValidate.QueryMethod, DEFAULT_QUERY_METHOD, CRLF, CRLF)
+		return TEMPFAIL, errors.New("query method for domain key not supported")
+	}
+	domainKeyLocation := fmt.Sprintf("%s._domainkey.%s", dkimSignatureToValidate.Selector, dkimSignatureToValidate.SigningDomainIdentifier)
+	records, err := net.LookupTXT(domainKeyLocation)
+	if err != nil {
+		fmt.Printf("failed to retreive domain key from location %s: %s%s%s", domainKeyLocation, err.Error(), CRLF, CRLF)
+		return TEMPFAIL, errors.New("error quering domain key")
+	}
+	numRecords := len(records)
+	if numRecords == 0 {
+		// per https://datatracker.ietf.org/doc/html/rfc6376#section-6.1.2 item #3
+		fmt.Printf("no txt record exists%s%s", CRLF, CRLF)
+		return PERMFAIL, errors.New("query for domain key returned no results")
+	}
+	// per https://datatracker.ietf.org/doc/html/rfc6376#section-6.1.2 item #4 we are opting to only try the first record.
+	// the RFC leaves it to the implementers discression
+
+	// parse domain key from record return from query method
+	domainKey, err := ParseDomainKey([]byte(records[0]))
+	if err != nil {
+		// per https://datatracker.ietf.org/doc/html/rfc6376#section-6.1.2 item #5
+		fmt.Printf("failed to parse domain key: %s%s%s", err.Error(), CRLF, CRLF)
+		return PERMFAIL, err
+	}
+	// per https://datatracker.ietf.org/doc/html/rfc6376#section-6.1.2 item # 6
+	// TODO: finish verifier steps...
+	// check for y flag and return TEMPFAIL if it is present. Per https://datatracker.ietf.org/doc/html/rfc6376#section-3.6.1
+	if domainKey.Flags.ContainsFlag(DomainKeyFlag_Y) {
+		fmt.Printf("failing signature validation because y flag is present in domain key%s%s", CRLF, CRLF)
+		return TEMPFAIL, errors.New("y flag present in dkim signature")
+	}
+	// https://datatracker.ietf.org/doc/html/rfc5451 looks like the value of i= for the DKIM-Signatures is stored over there...
+	if domainKey.Flags.ContainsFlag(DomainKeyFlag_S) {
+		// perform S flag validation.
+		if len(dkimSignatureToValidate.AgentUserIdentifier) > 0 {
+			domainPortionOfAUID := strings.Split(dkimSignatureToValidate.AgentUserIdentifier, "@")
+			if len(domainPortionOfAUID) < 2 || domainPortionOfAUID[1] != dkimSignatureToValidate.SigningDomainIdentifier {
+				fmt.Printf("AUID doamin %s does not match %s%s%s", domainPortionOfAUID[1], dkimSignatureToValidate.SigningDomainIdentifier, CRLF, CRLF)
+				return PERMFAIL, errors.New("auid does not match sdid")
+			}
+		} else {
+			// per https://datatracker.ietf.org/doc/html/rfc6376#section-3.6.1
+			/*
+				s  Any DKIM-Signature header fields using the "i=" tag MUST have
+				         the same domain value on the right-hand side of the "@" in the
+				         "i=" tag and the value of the "d=" tag.  That is, the "i="
+				         domain MUST NOT be a subdomain of "d=".  Use of this flag is
+				         RECOMMENDED unless subdomaining is required.
+			*/
+			// from reading the RFC I am not sure if we should extract the i field from the Authentication-Results header
+			// https://datatracker.ietf.org/doc/html/rfc5451 Authentication-Results header
+			fmt.Printf("skipping S flag AUID Signing Domain verification because i= flag is not populated on DKIM signature%s%s", CRLF, CRLF)
+		}
+	}
+	canonicalizedDKIMSignature, err := canonicalizeHeader(dkimSignatureToValidate.Header, dkimSignatureToValidate.CanonicalizationAlgorithm)
+	dkimSigForVerification := GetDKIMSignatureForVerificationOrSigningBytes(canonicalizedDKIMSignature)
+	if err != nil {
+		fmt.Printf("failed to canonicalize dkim signature for verification: %s%s%s", err.Error(), CRLF, CRLF)
+		return PERMFAIL, errors.New("invalid dkim signature for canonicalization")
+	}
+	var computedSignatureHash []byte
+	// var computedSignatureHashBase64 string
+	var hashAlgo crypto.Hash
+	hashBuffer := make([]byte, 0)
+	hashBuffer = append(hashBuffer, canonicalizedHeaderData...)
+	hashBuffer = append(hashBuffer, dkimSigForVerification...)
+	// hashBuffer = append(hashBuffer, canonicalizedBodyHash...)
+	switch dkimSignatureToValidate.Algorithm {
+	case DKIM_ALGORITHM_RSA_SHA_1:
+		hashAlgo = crypto.SHA1
+		hash := sha1.Sum(hashBuffer)
+		computedSignatureHash = hash[:]
+		// computedSignatureHashBase64 = base64.StdEncoding.EncodeToString(canonicalizedBodyHash)
+	case DKIM_ALGORITHM_RSA_SHA_256:
+		hashAlgo = crypto.SHA256
+		hash := sha256.Sum256(hashBuffer)
+		computedSignatureHash = hash[:]
+		// computedSignatureHashBase64 = base64.StdEncoding.EncodeToString(canonicalizedBodyHash)
+	}
+	signatureToValidateBytes, err := base64.StdEncoding.DecodeString(dkimSignatureToValidate.Signature)
+	if err != nil {
+		fmt.Printf("failed to decode dkim signature from base 64%s%s", CRLF, CRLF)
+		return TEMPFAIL, errors.New("failed to decode dkim signaure")
+	}
+	publicKeyBytes, err := base64.StdEncoding.DecodeString(domainKey.PublicKey)
+	if err != nil {
+		fmt.Printf("failed to decode public key from base 64%s%s", CRLF, CRLF)
+		return TEMPFAIL, errors.New("failed to base64 decode public key")
+	}
+	publicKeyInterface, err := x509.ParsePKIXPublicKey(publicKeyBytes)
+	if err != nil {
+		log.Println("Could not parse DER encoded public key (encryption key)")
+		return PERMFAIL, errors.New("invalid public key")
+	}
+	publicKey, isRSAPublicKey := publicKeyInterface.(*rsa.PublicKey)
+	if !isRSAPublicKey {
+		log.Println("Public key parsed is not an RSA public key")
+		return PERMFAIL, errors.New("invalid public key")
+	}
+	err = rsa.VerifyPKCS1v15(publicKey, hashAlgo, computedSignatureHash, signatureToValidateBytes)
+	if err != nil {
+		fmt.Printf("Error from verification: %s%s", err, CRLF)
+		return PERMFAIL, errors.New("verification failed")
+	}
+	return SUCCESS, nil
 }
